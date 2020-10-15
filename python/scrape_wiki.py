0aa150c24e4ddbc17e0318b090f95a09f43799a6
Won Lee <PSYC-wl8856@PSYC-D08049.austin.utexas.edu>
2020-10-15 13:33:23 -0500

    scraped wiki with python

---
+++ python/scrape_wiki.py
@@ -0,0 +1,9 @@
+# https://stackoverflow.com/questions/54890708/scraping-data-from-wikipedia-table
+import pandas as pd
+
+url='https://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_elevation'
+
+df=pd.read_html(url, header=0)[0]
+print(df.head)
+df.to_csv('data/us_states_by_elevation.csv',index = False, header = True)
+
